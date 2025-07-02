2230d2b02d90104c9eb9432be07051e3a3b771a1
bho011 <120661190+bho011@users.noreply.github.com>
2025-07-03 01:42:23 +0200

    Update user.py

---
+++ user.py
@@ -0,0 +1,72 @@
+kunden = {
+    "KND0001": {
+        "vorname": "Imelda",
+        "nachname": "Stahr",
+        "alter": 32,
+        "adresse": "Swen-Harloff-Allee 73, 55082 Wanzleben",
+        "geburtsdatum": "2002-10-16"
+    },
+    "KND0002": {
+        "vorname": "Antonios",
+        "nachname": "Kaul",
+        "alter": 19,
+        "adresse": "Hanns-Ullrich-Platz 33, 60686 Brandenburg",
+        "geburtsdatum": "2005-10-14"
+    },
+    "KND0003": {
+        "vorname": "Melissa",
+        "nachname": "Hörle",
+        "alter": 73,
+        "adresse": "Holstenweg 6/3, 94886 Saarlouis",
+        "geburtsdatum": "1951-05-19"
+    },
+    "KND0004": {
+        "vorname": "Carmela",
+        "nachname": "Weinhold",
+        "alter": 36,
+        "adresse": "Hanne-Kitzmann-Gasse 55, 58380 Bogen",
+        "geburtsdatum": "1988-06-03"
+    },
+    "KND0005": {
+        "vorname": "Solveig",
+        "nachname": "Wähner",
+        "alter": 55,
+        "adresse": "Trubstr. 8/6, 87543 Schwäbisch Gmünd",
+        "geburtsdatum": "1969-02-21"
+    },
+    "KND0006": {
+        "vorname": "Dennis",
+        "nachname": "Wolter",
+        "alter": 27,
+        "adresse": "Lortzingstraße 25, 23556 Lübeck",
+        "geburtsdatum": "1997-11-03"
+    },
+    "KND0007": {
+        "vorname": "Benedikt",
+        "nachname": "Güttler",
+        "alter": 41,
+        "adresse": "Falkenring 14, 78120 Furtwangen",
+        "geburtsdatum": "1983-03-30"
+    },
+    "KND0008": {
+        "vorname": "Janna",
+        "nachname": "Drescher",
+        "alter": 23,
+        "adresse": "Am Kalkberg 7, 99706 Sondershausen",
+        "geburtsdatum": "2001-04-09"
+    },
+    "KND0009": {
+        "vorname": "Serhat",
+        "nachname": "Baki",
+        "alter": 66,
+        "adresse": "Friesenstraße 9, 50354 Hürth",
+        "geburtsdatum": "1958-07-11"
+    },
+    "KND0010": {
+        "vorname": "Gabriele",
+        "nachname": "Thiemann",
+        "alter": 60,
+        "adresse": "Löwenstr. 12, 60385 Frankfurt",
+        "geburtsdatum": "1964-12-02"
+    }
+}
